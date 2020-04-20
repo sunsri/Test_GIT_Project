ce458c808c7cc1cb30d40be4ea77f7223e74a63f
sunsri <s.sunetha@gmail.com>
2020-04-20 02:36:34 +0100

    changes made prg2.py(D3) in DEv_branch

--- prg2.py
+++ prg2.py
@@ -9,6 +9,7 @@ num2 = 16.3
 
 # Add two numbers
 sum = float(num1) + float(num2)
-
+sum1=int(num1)+int(num2)
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum1))
