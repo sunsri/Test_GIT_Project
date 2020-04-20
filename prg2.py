673dfa40ad4ff02c6d2f9a3cb76acd08597f5995
sunsri <s.sunetha@gmail.com>
2020-04-20 04:05:48 +0100

    added Release(R2) in RELEASE_branch

--- prg2.py
+++ prg2.py
@@ -4,8 +4,9 @@ print('Hello, D2 D2 world!.....')
 ###==============================================================
 # This program adds two numbers
 
-num1 = 23
+num1 = 25
 num2 = 16
+num3=20
 
 # Add two numbers
 sum = float(num1) + float(num2)
@@ -13,7 +14,9 @@ sum1=int(num1)+int(num2)
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum1))
-=======
-print('Multiplication =' int(num1*num2))
-print('Division='int(num1/num2))
+#=======
+mul=int(num1*num2*num3)
+
+print('Multiplication=',mul)
+print('Division=',int((num1*num3)/num2))
 ###==============================================================
